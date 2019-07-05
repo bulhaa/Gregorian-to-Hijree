5e408a84048dfd5a66006c4e2cbe5032a3097855
Hammadh <freestyle.reunion@gmail.com>
2019-07-05 18:37:33 +0500

    Create data.sql

---
+++ data.sql
@@ -0,0 +1,9 @@
+--
+-- Dumping data for table `hijree_years`
+--
+
+INSERT INTO `hijree_years` (`year`, `year_start`, `year_end`, `days_in_month_1`, `days_in_month_2`, `days_in_month_3`, `days_in_month_4`, `days_in_month_5`, `days_in_month_6`, `days_in_month_7`, `days_in_month_8`, `days_in_month_9`, `days_in_month_10`, `days_in_month_11`, `days_in_month_12`) VALUES
+(1439, '2017-12-19', '2018-09-10', 0, 0, 0, 30, 30, 29, 30, 29, 30, 29, 29, 30),
+(1440, '2018-09-11', '2019-09-11', 29, 30, 29, 30, 30, 30, 29, 30, 29, 30, 29, 0);
+
+-- --------------------------------------------------------
